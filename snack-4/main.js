bfc6b6f68b42c00cccfd1e029a53d4d7f4da8c78
samian2002 <samiantariq14@gmail.com>
2025-02-13 12:58:36 +0100

    array per i pari

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,8 +1,14 @@
 let dispari = []
+let pari = []
+
 for (let i = 0; i < 6; i++) {
-    let numero = Number(prompt('Inserisci un numero'))
-    if (numero % 2 !== 0) 
-        dispari.push(numero)
+    let numero = Number(prompt('Inserisci un numero'));
+    if (numero % 2 !== 0) {
+        dispari.push(numero);
+    } else {
+        pari.push(numero);
+    }
 }
 
 console.log('I numeri dispari sono: ', dispari)
+console.log('I numeri pari sono:', pari)
