532ba617ebdce310c2d83343997a2eb5b542eb52
samian2002 <samiantariq14@gmail.com>
2025-02-13 10:35:11 +0100

    blocco1

---
+++ main.js
@@ -0,0 +1,8 @@
+let numberOne = prompt('scrivi il primo numero')
+let numberTwo = prompt('scrivi il secondo numero')
+
+if (numberOne > numberTwo) {
+    console.log('il numero maggiore è ' + numberOne)
+} else if (numberOne < numberTwo) {
+    console.log('il numero maggiore è ' + numberTwo)
+}
